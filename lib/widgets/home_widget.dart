0f473b72649b8800b9e9754cecb20cbf9418a0ac
Amr Abdelmagid <amrabdelmagid900@gmail.com>
2021-08-21 17:59:46 +0200

    Implement payment intent operations

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -4,8 +4,8 @@ import 'package:stripe_payment/stripe_payment.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:stripe_test/services/stripe_operations/payment_method.dart';
+import 'package:stripe_test/widgets/dialog.dart';
 
 class MyHomePage extends StatefulWidget {
   final String title;
@@ -20,6 +20,11 @@ class _MyHomePageState extends State<MyHomePage> {
   // Prepare the data
   String text = 'Click the button to start the payment';
   bool showSpinner = false;
+  double totalCost = 10.0;
+  double tip = 1.0;
+  double tax = 0.0;
+  double taxPercent = 0.2;
+  int amount = 0;
   String url =
       'https://us-central1-demostripe-b9557.cloudfunctions.net/StripePI';
 
@@ -37,10 +42,195 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  final pm = PaymentMethodOperations();
+// check if device is native pay ready
+  void checkIfNativePayReady(context) async {
+    print('started to check if native pay ready');
+    bool? deviceSupportNativePay =
+        await StripePayment.deviceSupportsNativePay();
+    bool? isNativeReady = await StripePayment.canMakeNativePayPayments(
+        ['american_express', 'visa', 'maestro', 'master_card']);
+    deviceSupportNativePay! && isNativeReady!
+        ? createPaymentMethodNative(context)
+        : createPaymentMethod(context);
+  }
 
-  void runPM() {
-    pm.checkIfNativePayReady(context);
+  // create native Stripe payment method (is customized here for Android)
+  Future<void> createPaymentMethodNative(context) async {
+    print('started NATIVE payment...');
+    StripePayment.setStripeAccount('');
+    List<LineItem> items = [];
+    items.add(LineItem(
+      description: 'Demo Order',
+      totalPrice: totalCost.toString(),
+    ));
+    if (tip != 0.0)
+      items.add(LineItem(
+        description: 'Tip',
+        totalPrice: tip.toString(),
+      ));
+    if (taxPercent != 0.0) {
+      tax = ((totalCost * taxPercent) * 100).ceil() / 100;
+      items.add(LineItem(
+        description: 'Tax',
+        totalPrice: tax.toString(),
+      ));
+    }
+    items.add(LineItem(
+      description: 'Vendor A',
+      totalPrice: (totalCost + tip + tax).toString(),
+    ));
+    amount = ((totalCost + tip + tax) * 100).toInt();
+    print('amount in pence/cent which will be charged = $amount');
+    //step 1: add card
+    PaymentMethod? paymentMethod = PaymentMethod();
+    Token token = await StripePayment.paymentRequestWithNativePay(
+      androidPayOptions: AndroidPayPaymentRequest(
+        totalPrice: (totalCost + tax + tip).toStringAsFixed(2),
+        currencyCode: 'GBP',
+      ),
+      applePayOptions: ApplePayPaymentOptions(
+        countryCode: 'GB',
+        currencyCode: 'GBP',
+        items: [],
+      ),
+    );
+    paymentMethod = await StripePayment.createPaymentMethod(
+      PaymentMethodRequest(
+        card: CreditCard(
+          token: token.tokenId,
+        ),
+      ),
+    );
+    paymentMethod != null
+        ? processPaymentAsDirectCharge(paymentMethod)
+        : showDialog(
+            context: context,
+            builder: (BuildContext context) => ShowDialogToDismiss(
+                title: 'Error',
+                content:
+                    'It is not possible to pay with this card. Please try again with a different card',
+                buttonText: 'CLOSE'));
+  }
+
+  Future<void> createPaymentMethod(context) async {
+    StripePayment.setStripeAccount('');
+    tax = ((totalCost * taxPercent) * 100).ceil() / 100;
+    amount = ((totalCost + tip + tax) * 100).toInt();
+    print('amount in pence/cent which will be charged = $amount');
+    //step 1: add card
+    PaymentMethod? paymentMethod = PaymentMethod();
+    paymentMethod = await StripePayment.paymentRequestWithCardForm(
+      CardFormPaymentRequest(),
+    ).then((PaymentMethod paymentMethod) {
+      return paymentMethod;
+    }).catchError((e) {
+      print('Errore Card: ${e.toString()}');
+    });
+    paymentMethod != null
+        ? processPaymentAsDirectCharge(paymentMethod)
+        : showDialog(
+            context: context,
+            builder: (BuildContext context) => ShowDialogToDismiss(
+                title: 'Error',
+                content:
+                    'It is not possible to pay with this card. Please try again with a different card',
+                buttonText: 'CLOSE'));
+  }
+
+  Future<void> processPaymentAsDirectCharge(PaymentMethod paymentMethod) async {
+    setState(() {
+      showSpinner = true;
+    });
+    //step 2: request to create PaymentIntent, attempt to confirm the payment & return PaymentIntent
+    final http.Response response = await http.post(Uri.parse(
+        '$url?amount=${amount}&currency=GBP&paym=${paymentMethod.id}'));
+    print('Now i decode');
+    if (response.body != null && response.body != 'error') {
+      final paymentIntentX = jsonDecode(response.body);
+      final status = paymentIntentX['paymentIntent']['status'];
+      final strAccount = paymentIntentX['stripeAccount'];
+      //step 3: check if payment was succesfully confirmed
+      if (status == 'succeeded') {
+        //payment was confirmed by the server without need for futher authentification
+        StripePayment.completeNativePayRequest();
+        setState(() {
+          text =
+              'Payment completed. ${paymentIntentX['paymentIntent']['amount'].toString()}p succesfully charged';
+          showSpinner = false;
+        });
+      } else {
+        //step 4: there is a need to authenticate
+        StripePayment.setStripeAccount(strAccount);
+        await StripePayment.confirmPaymentIntent(PaymentIntent(
+                paymentMethodId: paymentIntentX['paymentIntent']
+                    ['payment_method'],
+                clientSecret: paymentIntentX['paymentIntent']['client_secret']))
+            .then(
+          (PaymentIntentResult paymentIntentResult) async {
+            //This code will be executed if the authentication is successful
+            //step 5: request the server to confirm the payment with
+            final statusFinal = paymentIntentResult.status;
+            if (statusFinal == 'succeeded') {
+              StripePayment.completeNativePayRequest();
+              setState(() {
+                showSpinner = false;
+              });
+            } else if (statusFinal == 'processing') {
+              StripePayment.cancelNativePayRequest();
+              setState(() {
+                showSpinner = false;
+              });
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => ShowDialogToDismiss(
+                      title: 'Warning',
+                      content:
+                          'The payment is still in \'processing\' state. This is unusual. Please contact us',
+                      buttonText: 'CLOSE'));
+            } else {
+              StripePayment.cancelNativePayRequest();
+              setState(() {
+                showSpinner = false;
+              });
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => ShowDialogToDismiss(
+                      title: 'Error',
+                      content:
+                          'There was an error to confirm the payment. Details: $statusFinal',
+                      buttonText: 'CLOSE'));
+            }
+          },
+          //If Authentication fails, a PlatformException will be raised which can be handled here
+        ).catchError((e) {
+          //case B1
+          StripePayment.cancelNativePayRequest();
+          setState(() {
+            showSpinner = false;
+          });
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => ShowDialogToDismiss(
+                  title: 'Error',
+                  content:
+                      'There was an error to confirm the payment. Please try again with another card',
+                  buttonText: 'CLOSE'));
+        });
+      }
+    } else {
+      //case A
+      StripePayment.cancelNativePayRequest();
+      setState(() {
+        showSpinner = false;
+      });
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => ShowDialogToDismiss(
+              title: 'Error',
+              content:
+                  'There was an error in creating the payment. Please try again with another card',
+              buttonText: 'CLOSE'));
+    }
   }
 
   @override
@@ -57,7 +247,7 @@ class _MyHomePageState extends State<MyHomePage> {
             width: 90.0,
             child: ElevatedButton(
               onPressed: () {
-                pm.checkIfNativePayReady(context);
+                checkIfNativePayReady(context);
               },
               child: Text('Pay 10\$'),
             ),
@@ -75,56 +265,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-}
-
-class ShowDialogToDismiss extends StatelessWidget {
-  final String content;
-  final String title;
-  final String buttonText;
-  ShowDialogToDismiss(
-      {required this.title, required this.buttonText, required this.content});
-  @override
-  Widget build(BuildContext context) {
-    if (!Platform.isIOS) {
-      return AlertDialog(
-        title: new Text(
-          title,
-        ),
-        content: new Text(
-          this.content,
-        ),
-        actions: <Widget>[
-          new TextButton(
-            child: new Text(
-              buttonText,
-            ),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    } else {
-      return CupertinoAlertDialog(
-          title: Text(
-            title,
-          ),
-          content: new Text(
-            this.content,
-          ),
-          actions: <Widget>[
-            CupertinoDialogAction(
-              isDefaultAction: true,
-              child: new Text(
-                buttonText[0].toUpperCase() +
-                    buttonText.substring(1).toLowerCase(),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            )
-          ]);
-    }
   }
 }
